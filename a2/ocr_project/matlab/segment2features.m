646e5a56a6e1e8415ee5af314283cc9cb282d818
Ludde <ludde.llj@gmail.com>
2020-09-27 14:15:50 +0200

    may need some fixing..

--- a2/ocr_project/matlab/segment2features.m
+++ a2/ocr_project/matlab/segment2features.m
@@ -1,36 +1,53 @@
-clc
-clear all
-close all
-datadir = '../datasets/short1';
+% clc
+% clear all
+% close all
+% datadir = '../datasets/short1';
+% 
+% a = dir(datadir);
+% 
+% file = 'im2';
+% 
+% fnamebild = [datadir filesep file '.jpg'];
+% fnamefacit = [datadir filesep file '.txt'];
+% 
+% bild = imread(fnamebild);
+% fid = fopen(fnamefacit);
+% facit = fgetl(fid);
+% fclose(fid);
+% figure()
+% imshow(bild)
+% 
+% S = im2segment(bild);
+% B = S{randperm(5)};
+% B = S{4};
+% for i=1 : 5
+%     x = segment2features2(S{i})
+% end
 
-a = dir(datadir);
 
-file = 'im2';
+function features = segment2features(I)
 
-fnamebild = [datadir filesep file '.jpg'];
-fnamefacit = [datadir filesep file '.txt'];
+% Sum of pixels
+f1PixelSum    = 0; 
 
-bild = imread(fnamebild);
-fid = fopen(fnamefacit);
-facit = fgetl(fid);
-fclose(fid);
+% Moment of image
+f2Moment      = 0; 
 
-S = im2segment(bild);
-%B = S{randperm(5)};
-B = S{4};
-for i=1 : 5
-    x = segment2features2(S{i});
-end
+% Nbr of circles or holes in segment
+% example : 0,4,6,9 got one and 8 got two
+f3Circles     = 0; 
 
+% Difference of pixels in the top and bottom half
+% example : 6 and 9 or symmetry of 0,3,8 and 1
+f4PixelDiffV  = 0;  
 
-function features = segment2features2(I)
+% Difference of pixels in the left and right half 
+% example : symmetrical numers as 1,8 and 0 or asymmetry of the others
+f5PixelDiffH  = 0; 
 
-f1PixelSum   = 0; % Sum of pixels
-f2Moment     = 0; % Symmetry of image
-f3Circles    = 0; % Nbr of circles
-f4PixelDiffV = 0; % Difference of pixels in the top and bottom half 
-f5PixelDiffH = 0; % Difference of pixels in the left and right half 
-f6Ratio      = 0; % Ratio?
+% Pixels at border
+% example : 7, 5, 2 got more border contact
+f6PixelSumB   = 0;
 
 
 
@@ -40,8 +57,8 @@ f1PixelSum  = nnz(im);
 f2Moment    = mean(moment(im,3));
 f3Circles   = bweuler(im);
 
-[i, j] = size(im);
 
+[i, j] = size(im);
 hi = round(i/2);
 pixelSumT = nnz(im(1:hi,:));
 pixelSumB = nnz(im(hi+1:i,:));
@@ -52,13 +69,26 @@ pixelSumL = nnz(im(:,1:hj));
 pixelSumR = nnz(im(:,hj+1:j));
 f5PixelDiffH = pixelSumL - pixelSumR;
 
-f6Ratio = i/j
+f6PixelSumB = f6PixelSumB + nnz(im(1,:));
+f6PixelSumB = f6PixelSumB + nnz(im(i,:));
+f6PixelSumB = f6PixelSumB + nnz(im(:,1));
+f6PixelSumB = f6PixelSumB + nnz(im(:,j));
 
+%figure()
+%imagesc(im)
 
-figure()
-imagesc(im)
+features = [f1PixelSum , f2Moment , f3Circles, f4PixelDiffV, f5PixelDiffH, f6PixelSumB];
+% I tried using the image itself as features but it was hard to tell if it was a good idea. 
+%im = padImage(im);
+%features = im(:);
+end
 
-features = [f1PixelSum , f2Moment , f3Circles, f4PixelDiffV, f5PixelDiffH, f6Ratio];
+% Adds padding to image and make it 30x30
+function im = padImage(im)
+[i, j] = size(im);
+im = padarray(im,[round((30-i)/2) round((30-j)/2)],0,'both');
+im = im(1:30,1:30);
+
 end
 
 % Centers the image
